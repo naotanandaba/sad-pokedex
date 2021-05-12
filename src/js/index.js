246e89027a5328cca7308dad03240797935143b2
Xavi <xguinotv@gmail.com>
2021-05-12 22:45:32 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const fetchPokemon = () => {
+    const url = `https://pokeapi.co/api/v2/pokemon/1`;
+    fetch(url)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            const pokemon = {};
+            pokemon['name'] = data.name;
+            pokemon['id'] = data.id;
+            pokemon['image'] = data.sprites['front_default'];
+            data.types.forEach((type) => {
+                pokemon['type'] = pokemon['type'] + ", " + type.name;
+            })
+            console.log(pokemon);
+        });
+}
+
+fetchPokemon();
