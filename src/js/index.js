99a64211d239da21456f11a2fe653b29e811d921
Xavi <xguinotv@gmail.com>
2021-05-12 22:03:33 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
-const URL_ALL_POKEMON = 'https://pokeapi.com/api/v2/pokemon?limit=151'; // URL obtain all pokemon 
-
-let allPokemonInfo = []; // Array to save all info
-
-let pokedes = []; //
+// URL obtain all pokemon
+const URL_ALL_POKEMON = 'https://pokeapi.co/api/v2/pokemon';
+// Array to save all info
+let allPokemonInfo = [];
+// Array pokedex
+let pokedex = [];
+// Container for scss
+const pokeContainer = document.getElementById('pokeContainer');
+// number of pokemons in our pokedex
+const numberPokemon = 151;
+// Get Pokemon Api info
+const getPokemon = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    console.log(pokemon);
+}
+getPokemon(1);
