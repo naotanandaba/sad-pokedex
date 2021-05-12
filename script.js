54bb6436d7362217a0d15920ca62c44f13c58cba
Xavi <xguinotv@gmail.com>
2021-05-12 23:12:44 +0200

    init

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+
+const make1PokemonEntry = (id) => {
+    const divisio = document.createElement('DIV');
+    divisio.classList.add('pokemon');
+    const photoEntry = document.createElement('PICTURE');
+    const image = document.createElement('IMG');
+    image.setAttribute('src', `assets/images/${id}.webp`);
+    photoEntry.appendChild(image);
+    divisio.appendChild(photoEntry);
+    document.body.appendChild(divisio);
+};
+
+make1PokemonEntry(1);
+
+const getAllPokemon = async () => {
+    const pokemonURL = `https://pokeapi.co/api/v2/pokemon?limit=151`;
+    const request = await fetch(pokemonURL);
+    pokesJSON = await request.json();
+    allPokemonInfo = [...pokesJSON.results];
+    console.log(allPokemonInfo);
+}
+
+getAllPokemon();
+
 const fetchPokemon = () => {
     const url = `https://pokeapi.co/api/v2/pokemon/1`;
     fetch(url)
