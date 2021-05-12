aeda18df6848a5b8cce24f8ba1ecb00ad917f33c
Xavi <xguinotv@gmail.com>
2021-05-12 20:08:34 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
-const URL_ALL_POKEMON = 'https://pokeapi.com/api/v2/pokemon?limit=151';
+const URL_ALL_POKEMON = 'https://pokeapi.com/api/v2/pokemon?limit=151'; // URL obtain all pokemon 
+
+let allPokemonInfo = []; // Array to save all info
+
+let pokedes = []; //
