bdaff88add49677fdd9cc657d284a742242ef17f
Xavi <xguinotv@gmail.com>
2021-05-13 00:10:32 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const numPokemon = 151;
 
 const make1PokemonEntry = (id) => {
     const divisio = document.createElement('DIV');
@@ -24,20 +24,22 @@ const getAllPokemon = async () => {
 getAllPokemon();
 
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/1`;
-    fetch(url)
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            console.log(data);
-            const pokemon = {};
-            pokemon['name'] = data.name;
-            pokemon['id'] = data.id;
-            pokemon['image'] = data.sprites['front_default'];
-            pokemon['type'] = data.types.map(type => type.type.name).join(", ")
-            console.log(pokemon);
-        });
-}
+
+    const promises = [];
+    for(let i = 1; i < numPokemon; i++){
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    
+    Promise.all(promises).then(results =>{
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map(type => type.type.name).join(", ")
+        }));
+        console.log(pokemon);
+    });
+};
 
 fetchPokemon();
