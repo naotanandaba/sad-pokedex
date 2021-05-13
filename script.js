a7adaf9ecc3abf0b07257af6cdb4497deeb51705
Xavi <xguinotv@gmail.com>
2021-05-13 14:47:05 +0200

    commit

--- script.js
+++ script.js
@@ -28,35 +28,52 @@ const pokedex = document.getElementById("pokedex");
 
 console.log(pokedex);
 
-const fetchPokemon = () => {
-
-    const promises = [];
-    for(let i = 1; i < numPokemon; i++){
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
-    }
-    
-    Promise.all(promises).then(results =>{
-        const pokemon = results.map((data) => ({
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-            type: data.types.map(type => type.type.name).join(", ")
+const fetchPokemon = async () => {
+        const url = `https://pokeapi.co/api/v2/pokemon?limit=${numPokemon}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        const pokemon = data.results.map((result, index) => 
+        ({
+            ...result,
+            id: index +1,
+            image:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png` 
         }));
         displayPokemon(pokemon);
-    });
-};
+    };
 
-const displayPokemon = (pokemon) => {
-    console.log(pokemon);
-    const pokemonHTMLString = pokemon.map(pokemon => `
-    <li class = "card">
-        <img class="card-image" src="${pokemon.image}"/>
-        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-        <p class="card-subtitle">Type: ${pokemon.type}</p>
-    </li>
-    `).join('');
-    pokedex.innerHTML = pokemonHTMLString;
+    const displayPokemon = (pos, pokemon) => {
+        const li = document.createElement('LI');
+        li.classList.add('card');
+        const img = document.createElement('IMG');
+        img.classList.add('card-image');
+        img.setAttribute('src', `${pokemon[pos].image}`);
+        const h2 = document.createElement('H2');
+        h2.classList.add('card-title');
+        h2.textContent = `${pokemon[pos].id}. ${pokemon[pos].name}`;
+        const p = document.createElement('P');
+    
+        if (pokemon[pos].type === 'planta') {
+            p.classList.add('planta');
+        }
+        p.textContent = `Type: ${pokemon[pos].type}`;
+        li.appendChild(img);
+        li.appendChild(h2);
+        li.appendChild(p);
+        const pokedex = document.getElementById('pokedex');
+        pokedex.appendChild(li);
+}
+
+const selectPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPopUp(pokemon);
+    console.log(id);
+}
+
+const dispalyPopUP = (pokemon) => {
+    const type = pokemon.types.map(type => type.type.name).join(", ")
+    console.log(type)
 }
 
 fetchPokemon();
