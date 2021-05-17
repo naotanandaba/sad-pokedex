c28476e67a2eb1c16695b1eea45d59fe983eeb92
monti98 <jordimontiel11@gmail.com>
2021-05-17 17:00:41 +0200

    quiz

---
+++ quiz.js
@@ -0,0 +1,20 @@
+
+const respCorrectes = [2, 0, 1, 1, 2];
+
+for (let i = 1; i <= respCorrectes.length; i++) {
+    const q = document.getElementById('q' + i);
+    const resp = document.getElementsByName('q' + i);
+    const button = document.getElementById('check-button-' + i);
+    q.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (resp[respCorrectes[i - 1]].checked) {
+            button.classList.add('check-correct');
+            button.classList.remove('check-button');
+            button.classList.remove('check-wrong');
+        } else {
+            button.classList.add('check-wrong');
+            button.classList.remove('check-button');
+            button.classList.remove('check-correct');
+        }
+    })
+}
